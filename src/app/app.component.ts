272d2a000ff04bcc149ef94b90041442d4423362
fmarcelo86 <fmarcelo86@gmail.com>
2019-11-07 16:34:56 -0500

    Primera Version

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+declare var jQuery:any;
+declare var $: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'HulkStoreWeb';
+
+  public ngOnInit() {
+    $(document).ready(function () {
+      alert("Hola");
+    });
+  }
+}
