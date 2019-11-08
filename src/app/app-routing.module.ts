b49a101a6f27f5435f303f4a139a8abdc44d20c0
fmarcelo86 <fmarcelo86@gmail.com>
2019-11-07 23:00:06 -0500

    Mejoras 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 const routes: Routes = [
-  { path:'' , component: HomeComponent }
+  { path:'' , component: HomeComponent },
+  { path:'**' , redirectTo: '' }
 ];
 
 @NgModule({
