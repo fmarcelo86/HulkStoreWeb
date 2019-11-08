9f718b3cf6877b3b74db1084df2bdb602b5e09e5
fmarcelo86 <fmarcelo86@gmail.com>
2019-11-08 15:46:00 -0500

    Mejoras 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MarcaComponent } from './marca/marca.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { ClienteComponent } from './cliente/cliente.component';
 
 const routes: Routes = [
   { path:'' , component: HomeComponent },
+  { path:'marca' , component: MarcaComponent },
+  { path:'categoria' , component: CategoriaComponent },
+  { path:'cliente' , component: ClienteComponent },
   { path:'**' , redirectTo: '' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { HomeComponent } from './home/home.component';
+import { MarcaComponent } from './marca/marca.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { ClienteComponent } from './cliente/cliente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MarcaComponent,
+    CategoriaComponent,
+    ClienteComponent
   ],
   imports: [
     BrowserModule,
